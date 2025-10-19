7fa03564fc71eede7495cb644fbbadbd25817231
Rhys Lindhorst <lindhorst@chapman.edu>
2025-10-18 18:54:36 -0700

    removed the exit so the program runs through the examples

--- username.sh
+++ username.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+./username.sh#!/bin/bash
 # username.sh — validate each line of a file containing usernames.
 
 echo "Please enter a username that contains: only lower case letters, digits, and underscore characters. It should start with a lowercase letter and be longer than three characters but no more than twelve."
@@ -7,7 +7,7 @@ echo "Please enter a username that contains: only lower case letters, digits, an
 while read -r USERNAME; do
   if echo "$USERNAME" | grep -Eq "^[a-z][a-z0-9_]{2,11}$"; then
     echo "$USERNAME → Valid Username. Thank you!"
-    exit
+    #exit
   else
     echo "$USERNAME → Invalid Username. Please input again."
   fi
